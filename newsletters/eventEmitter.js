a1012d4dbcb7f3fb51f4b65201c37f12712dc39c
Madeleine Hodges <mfhodges@dhcp145.wired.library.upenn.edu>
2020-02-25 16:35:13 -0500

    added challenge to finish later

---
+++ newsletters/eventEmitter.js
@@ -0,0 +1,40 @@
+
+
+
+/*
+
+Write an event emitter that has three methods: on, emit, and removeListener.
+The `on` function takes in an event name and a callback function, the `emit` function takes in an event name and data (which will be passed to the associated callback), and `removeListener` takes in an event name and a callback to remove from that event.
+
+https://www.freecodecamp.org/news/how-to-code-your-own-event-emitter-in-node-js-a-step-by-step-guide-e13b7e7908e1/
+
+*/
+
+
+
+
+/* code */
+
+
+
+
+
+/* tests */
+
+
+
+
+
+/* Notes
+
+What is an event emitter? 
+https://www.freecodecamp.org/news/how-to-code-your-own-event-emitter-in-node-js-a-step-by-step-guide-e13b7e7908e1/
+
+
+
+
+*/
+
+
+
+
